6307c31d5013fe4c9eca401bee288e3093427dd9
Lea Grieder <lea@grieder.org>
2020-05-18 14:12:06 +0200

    m

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,7 @@ class QuestionsController < ApplicationController
   def answer
     @answers = ['Great!', 'Silly question, get dressed and go to work!',
                 'I don\'t care, get dressed and go to work!']
+
     if params[:question]
       if params[:question] == 'I am going to work'
         @answers = @answers[0]

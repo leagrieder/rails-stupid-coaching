28a7c989d38eec46712069c4adafb06449858b14
Lea Grieder <lea@grieder.org>
2020-05-18 12:52:10 +0200

    m

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @answers = ['Great!', 'Silly question, get dressed and go to work!',
+                'I don\'t care, get dressed and go to work!']
+    if params[:question]
+      if params[:question] == 'I am going to work'
+        @answers = @answers[0]
+      elsif params[:question].include?('?')
+        @answers = @answers[1]
+      else
+        @answers = @answers[2]
+      end
+    end
+  end
+end
